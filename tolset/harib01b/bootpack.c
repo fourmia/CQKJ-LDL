c0995281ae3baf8d869cf099d391df86c76b07ac
LiZedong <15516926476@163.com>
2022-04-08 16:02:01 +0800

    "test"

--- tolset/harib01b/bootpack.c
+++ tolset/harib01b/bootpack.c
@@ -1,7 +1,7 @@
 /*
  * @Author: LiZedong <15516926476@163.com>
  * @Date: 2022-04-01 10:14:25
- * @LastEditTime: 2022-04-01 11:36:44
+ * @LastEditTime: 2022-04-01 18:56:22
  * @LastEditors: LiZedong <15516926476@163.com>
  * @Description: 
  */
@@ -12,10 +12,11 @@ void write_mem8(int addr, int data);
 void HariMain(void)
 {
 	int i; /* �ϐ��錾�Bi�Ƃ����ϐ��́A32�r�b�g�̐����^ */
-	for (i = 0xa0000; i <= 0xaffff; i++) {
+	char *p;
+	p = 0xa0000;
+	for (i = 0; i <= 0xffff; i++) {
 		// write_mem8(i, i%16);
-		char *p = i;
-		*p = i % 16;
+		p[i] = i % 16;
 		// printf(&p);
 	}
 
